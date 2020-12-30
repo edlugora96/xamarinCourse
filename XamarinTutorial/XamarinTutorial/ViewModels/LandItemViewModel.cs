196b77951d5734fd61cf77fbec53835b95ae5091
Eduardo Gonzalez <edlu.gora96@gmail.com>
2020-12-30 07:33:26 -0400

    Tabbed Page

--- XamarinTutorial/XamarinTutorial/ViewModels/LandItemViewModel.cs
+++ XamarinTutorial/XamarinTutorial/ViewModels/LandItemViewModel.cs
@@ -22,7 +22,7 @@ namespace XamarinTutorial.ViewModels
         private async void SelectLand()
         {
             MainViewModel.GetInstance().Land = new LandViewModel(this);
-            await Application.Current.MainPage.Navigation.PushAsync(new LandPage());
+            await Application.Current.MainPage.Navigation.PushAsync(new LandTabbedPage());
         }
         #endregion
     }
